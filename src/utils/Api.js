a2ddcee89c75af580a00f47dacadd8aede3f60dd
VSBlazhev <vsblazhev@yandex.com>
2023-07-27 15:36:54 +0200

    server 2

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -126,7 +126,7 @@ export class Api {
   }
   
   const api = new Api({
-    baseUrl: 'http://158.160.110.0:3000',
+    baseUrl: 'blazhev.mov-exp.nomoreparties.sbs',
     headers: {
       "Content-Type": "application/json",
     },
